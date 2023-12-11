31993f896adfbafeee1f28497533b9238c38102a
Dedu <narcis.dedu@gmail.com>
2023-12-11 15:52:43 +0200

    Day 3 of 2015

--- aoc2015/aoc2015_3/main.go
+++ aoc2015/aoc2015_3/main.go
@@ -30,6 +30,7 @@ func main() {
   var xCoordinates = make([]int, (len(inputText)+1))
   var yCoordinates = make([]int, (len(inputText)+1))
 
+  // coordinates of starting house
   xCoordinates[len(inputText)] = 0
   yCoordinates[len(inputText)] = 0
 
@@ -71,4 +72,90 @@ func main() {
   }
 
   fmt.Println("Number of houses with at least one present: ", uniqueHouses)
+
+  // Part2
+
+  var xSanta int = 0
+  var ySanta int = 0
+  var xRobotSanta int = 0
+  var yRobotSanta int = 0
+
+  var xSantaCoordinates = make([]int, 0)
+  var ySantaCoordinates = make([]int, 0)
+ 
+  var xRobotSantaCoordinates = make([]int, 0)
+  var yRobotSantaCoordinates = make([]int, 0)
+  xSantaCoordinates = append(xSantaCoordinates, 0)
+  ySantaCoordinates = append(ySantaCoordinates, 0)
+
+  xRobotSantaCoordinates = append(xRobotSantaCoordinates, 0)
+  yRobotSantaCoordinates = append(yRobotSantaCoordinates, 0)
+
+
+  for i := 0; i < len(inputText); i++ {
+
+    if i%2 == 0 {
+
+     if string(inputText[i]) == ">" {
+        xSanta++
+      } else if string(inputText[i]) == "<" {
+        xSanta--
+      } else if string(inputText[i]) == "^" {
+       ySanta++
+      } else if string(inputText[i]) == "v" {
+        ySanta--
+      }
+    
+      xSantaCoordinates = append(xSantaCoordinates, xSanta)
+      ySantaCoordinates = append(ySantaCoordinates, ySanta)
+
+    } else if i%2 != 0 {
+      
+      if string(inputText[i]) == ">" {
+        xRobotSanta++
+      } else if string(inputText[i]) == "<" {
+        xRobotSanta--
+      } else if string(inputText[i]) == "^" {
+        yRobotSanta++
+      } else if string(inputText[i]) == "v" {
+        yRobotSanta--
+      }
+    
+      xRobotSantaCoordinates = append(xRobotSantaCoordinates, xRobotSanta)
+      yRobotSantaCoordinates = append(yRobotSantaCoordinates, yRobotSanta)
+    }
+  }
+
+  xVisitedHousesCoordinates := xSantaCoordinates
+  yVisitedHousesCoordinates := ySantaCoordinates
+
+  for i := 0; i < len(xRobotSantaCoordinates); i++ {
+
+    xVisitedHousesCoordinates = append(xVisitedHousesCoordinates, xRobotSantaCoordinates[i])
+    yVisitedHousesCoordinates = append(yVisitedHousesCoordinates, yRobotSantaCoordinates[i])
+  }
+  
+  var combinedUniqueVisitedHouses int = len(xVisitedHousesCoordinates)
+
+
+  for i := 0; i <len(xVisitedHousesCoordinates); i++ {
+
+    isDuplicate := false
+
+    for j := i; j <len(xVisitedHousesCoordinates); j++ {
+
+      if (xVisitedHousesCoordinates[i] == xVisitedHousesCoordinates[j] && 
+          yVisitedHousesCoordinates[i] == yVisitedHousesCoordinates[j] && 
+          i != j) {
+
+        isDuplicate = true
+      }
+    }
+
+    if isDuplicate {
+      combinedUniqueVisitedHouses--
+    }
+  }
+ 
+  fmt.Println("Santa and Robot Santa visited a number of unique houses: ", combinedUniqueVisitedHouses)
 }
